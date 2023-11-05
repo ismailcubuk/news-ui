1e2aedc87d20acd4dd30218a17bc63f17454b492
ismailcubuk <iismailcubuk@gmail.com>
2023-11-05 08:02:53 +0300

    finish

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,27 +6,27 @@ export default function Navbar() {
   return (
     <nav className="flex bg-blue-700 items-center h-12 px-5 justify-between">
         <MySVGComponent />
-      <div className="flex">
-        <div className="w-[436px] flex gap-4 text-white mr-4 my-2">
-          <button className="flex items-center">
+      <div className="flex items-center">
+        <div className="w-[436px] h-[20px] inline-flex gap-4 text-white mr-4 my-2">
+          <button className="flex items-center author">
             NEWS <MdKeyboardArrowDown />
           </button>
-          <button className="flex items-center">
+          <button className="flex items-center author">
             FEATURES <MdKeyboardArrowDown />
           </button>
-          <button className="flex items-center">
+          <button className="flex items-center author">
             TOPICS <MdKeyboardArrowDown />
           </button>
-          <button className="flex items-center">
+          <button className="flex items-center author">
             VIDEO <MdKeyboardArrowDown />
           </button>
-          <button className="flex items-center">
+          <button className="flex items-center author">
             <GoDotFill color="red" />
             LIVE
           </button>
         </div>
         <button className="w-12 h-12 bg-search flex justify-center items-center text-white">
-          <BsSearch />
+          <BsSearch/>
         </button>
       </div>
     </nav>
